c6ea5223b33880603e4ce6608186ddd6ecfe39a5
Abreu <abreu@disroot.org>
2024-11-26 16:16:06 -0300

    doc: added program template

--- CPU/ALU.vhdl
+++ CPU/ALU.vhdl
@@ -17,6 +17,7 @@ architecture Behaviour of ArithmeticLogicUnit is
 begin
     calculate : process (ops, operation)
         variable int_a, int_b, int_r : integer;
+        variable logic_r : std_logic_vector(inst_r);
     begin
         case operation is
             when ADD | SUB | CMP => -- Arithmetic operation
@@ -29,12 +30,13 @@ begin
                 zero <= '1' when int_r = 0 else '0';
                 signal_bit <= '1' when int_r < 0 else '0';
                 result <= std_logic_vector(to_signed(int_r, result'length));
-            when LAND =>
-                result <= ops(0) and ops(1);
-            when LOR =>
-                result <= ops(0) or ops(1);
-            when LNOT =>
-                result <= not ops(0);
+            when LAND | LOR | LNOT =>
+                with operation select
+                    logic_r := ops(0) and ops(1) when LAND,
+                    ops(0) or ops(1) when LOR,
+                    not ops(0) when others;
+                zero <= '1' when logic_r = (logic_r'range => '0') else '0';
+                result <= logic_r;
             when others => -- MOV, STORE, LOAD
                 result <= ops(1);
         end case;
--- CPU/quartusCPU/ArithmeticLogicUnit.vhd
+++ CPU/quartusCPU/ArithmeticLogicUnit.vhd
@@ -16,42 +16,48 @@ architecture Behaviour of ArithmeticLogicUnit is
 begin
     calculate : process (ops, operation)
         variable int_a, int_b, int_r : integer;
+        variable logic_r : std_logic_vector(inst_r);
     begin
         case operation is
             when ADD | SUB | CMP => -- Arithmetic operation
                 int_a := to_integer(signed(ops(0)));
                 int_b := to_integer(signed(ops(1)));
-					 if operation = ADD then
-						  int_r := int_a + int_b;
-					 else
-						  int_r := int_a - int_b;
-					 end if;
+                if operation = ADD then
+                    int_r := int_a + int_b;
+                else
+                    int_r := int_a - int_b;
+                end if;
 
                 -- Assign flags
-					 if int_r > 2 ** (inst_l - 1) - 1 then
-						  overflow <= '1';
-					 else
-						  overflow <= '0';
-					 end if;
-					 
-					 if int_r = 0 then
-						  zero <= '1';
-					 else
-						  zero <= '0';
-					 end if;
-					 
-					 if int_r < 0 then
-					     signal_bit <= '1';
-					 else
-						  signal_bit <= '0';
-					 end if;
+                if int_r > 2 ** (inst_l - 1) - 1 then
+                    overflow <= '1';
+                else
+                    overflow <= '0';
+                end if;
+
+                if int_r = 0 then
+                    zero <= '1';
+                else
+                    zero <= '0';
+                end if;
+
+                if int_r < 0 then
+                    signal_bit <= '1';
+                else
+                    signal_bit <= '0';
+                end if;
                 result <= std_logic_vector(to_signed(int_r, result'length));
-            when LAND =>
-                result <= ops(0) and ops(1);
-            when LOR =>
-                result <= ops(0) or ops(1);
-            when LNOT =>
-                result <= not ops(0);
+            when LAND | LOR | LNOT =>
+                case operation is
+                    when LAND =>
+                        logic_r := ops(0) and ops(1);
+                    when LOR =>
+                        logic_r := ops(0) or ops(1);
+                    when others =>
+                        logic_r := not ops(0);
+                end case;
+                zero <= '1' when logic_r = (logic_r'range => '0') else '0';
+                result <= logic_r;
             when others => -- MOV, STORE, LOAD
                 result <= ops(1);
         end case;
